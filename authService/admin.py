2e2ff8dc14879dfcdca219110989c497b384809d
Mohit Lahoti <engtechno25@gmail.com>
2025-04-01 17:24:58 +0530

    Update admin as well as settings for auth Service

--- authService/admin.py
+++ authService/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'isVerified', 'created_at', 'updated_at', 'is_active']
+
+class OTPVerification(admin.ModelAdmin):
+    list_display = ['id', 'user', 'otp', 'otp_expires_at']
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.OTPVerification, OTPVerification)
