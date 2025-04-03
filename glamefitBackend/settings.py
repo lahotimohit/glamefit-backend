12705c33cbce9fcfe1386842950624d48962867d
Mohit Lahoti <engtechno25@gmail.com>
2025-04-03 17:56:37 +0530

    Crete access token refresh route

--- glamefitBackend/settings.py
+++ glamefitBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'authService',
     'products',
@@ -152,9 +153,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),  # Change this value
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
